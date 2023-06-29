04da49d3e7d54b5706bdbd3ebbd08cae346129c9
HwangBoKyeoung <94347689+HwangBoKyeoung@users.noreply.github.com>
2023-06-29 21:12:57 +0900

    home controller / 경로 추가

--- travel/src/main/java/co/yg/travel/HomeController.java
+++ travel/src/main/java/co/yg/travel/HomeController.java
@@ -1,5 +1,19 @@
 package co.yg.travel;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home/home";
+	}
+	
+	@RequestMapping("/home.do")
+	public String home(Model model) {
+		return "home/home";
+	}
 }
